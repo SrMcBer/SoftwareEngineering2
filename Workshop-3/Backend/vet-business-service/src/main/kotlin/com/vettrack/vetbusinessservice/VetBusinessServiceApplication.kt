cd6924ad58b003bdc3494dbd6cd498125e33434d
Miguel Angel Buitrago Castillo <mbuitragoc@unal.edu.co>
2025-11-23 22:38:50 -0500

    feat: Initial project setup for vet-business-service

---
+++ Workshop-3/Backend/vet-business-service/src/main/kotlin/com/vettrack/vetbusinessservice/VetBusinessServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.vettrack.vetbusinessservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VetBusinessServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<VetBusinessServiceApplication>(*args)
+}
---
+++ Workshop-3/Backend/vet-business-service/src/test/kotlin/com/vettrack/vetbusinessservice/VetBusinessServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vettrack.vetbusinessservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VetBusinessServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
