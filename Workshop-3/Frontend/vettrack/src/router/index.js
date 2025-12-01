399584796934cc5226f76411635b0f2489dd14a8
Miguel Angel Buitrago Castillo <mbuitragoc@unal.edu.co>
2025-11-30 20:24:36 -0500

    feat: Started front end service with login and register

---
+++ Workshop-3/Frontend/vettrack/src/router/index.js
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from "vue-router";
+import { useAuthStore } from "../stores/auth";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import Index from "../views/Index.vue";
+
+const routes = [
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+    meta: { requiresGuest: true },
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+    meta: { requiresGuest: true },
+  },
+  {
+    path: "/",
+    name: "Index",
+    component: Index,
+    meta: { requiresAuth: true },
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+// Navigation guard
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+
+  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+    next("/login");
+  } else if (to.meta.requiresGuest && authStore.isAuthenticated) {
+    next("/");
+  } else {
+    next();
+  }
+});
+
+export default router;
